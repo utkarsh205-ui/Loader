c3e901596b0f6a438a8c82f8a40e61033cc18f3b
utkarsh205-ui <128224432+utkarsh205-ui@users.noreply.github.com>
2023-08-24 16:44:58 +0530

    Update loader.c
    made load and run function

--- without-bonus/loader.c
+++ without-bonus/loader.c
@@ -23,6 +23,63 @@ void load_and_run_elf(char **exe)
   printf("User _start return value = %d\n", result);
 }
 
+
+void load_and_run_elf(char **exe) {
+  fd = open(exe[1], O_RDONLY);
+  if (fd == -1) {
+    perror("open");
+    exit(EXIT_FAILURE);
+  }
+  ehdr = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
+  ssize_t sz = read(fd, ehdr, sizeof(Elf32_Ehdr));
+  if (!ehdr) {
+    perror("Failed to open the file");
+    return;
+  }
+
+  phdr = (Elf32_Phdr *)malloc(sizeof(Elf32_Phdr) * ehdr->e_phnum);
+  if (!phdr) {
+    perror("Failed to make the phdr table");
+    return;
+  }
+
+  ssize_t sz2 = read(fd, phdr, sizeof(Elf32_Phdr) * ehdr->e_phnum);
+  if (sz2 != sizeof(Elf32_Phdr) * ehdr->e_phnum) {
+    perror("not able to allocate");
+    free(phdr);
+    close(fd);
+    exit(EXIT_FAILURE);
+  }
+
+  int iter=ehdr->e_phnum;
+
+  for (int i = 0; i < iter; i++) {
+    if (phdr[i].p_type == PT_LOAD) {
+      // mmemory mmap ke through
+      void *address =mmap(NULL, phdr[i].p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC,MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+      if (address == MAP_FAILED) {
+        perror("mmap not been made");
+        exit(EXIT_FAILURE);
+      }
+      size_t start = ehdr->e_entry - phdr[i].p_vaddr;
+
+      // niche wala function is to load the memory p_filesz
+      pread(fd, address, phdr[i].p_filesz, phdr[i].p_offset);
+      void *entry= address + start;
+
+      if (entry >= address && entry < address + phdr[i].p_memsz) {
+        functocallfib _start = (functocallfib)entry;
+
+        int result = _start();
+        printf("result is %d\n", result);
+        break;
+      }
+      munmap(address,phdr[i].p_memsz);
+    }
+  }
+}
+
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
